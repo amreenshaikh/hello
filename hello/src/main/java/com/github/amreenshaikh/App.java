6ade4973d5d6f3413c0ee2d3194c8d0e2808252e
Amreen <amreen@reach1to1.com>
2014-07-16 15:33:07 +0530

    folder added

---
+++ hello/src/main/java/com/github/amreenshaikh/App.java
@@ -0,0 +1,13 @@
+package com.github.amreenshaikh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
